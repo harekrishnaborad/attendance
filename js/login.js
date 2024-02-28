43c184c066d27904d81786574eb1444d9d002c02
harekrishna <hrborad1796@gmail.com>
2024-02-28 12:59:03 +0530

    database added to login page

--- js/login.js
+++ js/login.js
@@ -1,26 +1,105 @@
-/*=============== SHOW HIDDEN - PASSWORD ===============*/
-const showHiddenPass = (loginPass, loginEye) =>{
-    const input = document.getElementById(loginPass),
-          iconEye =document.getElementById(loginEye)
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js'
+import { getDatabase, ref, onValue} from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js'
 
-    iconEye.addEventListener('click', () =>{
-        // Change password to text
-        if(input.type === 'password'){
-            // Switch to text
-            input.type = 'text'
+const appSetting = {
+    databaseURL: "https://attendance-system-c347e-default-rtdb.firebaseio.com/"
+}
 
-            // Icon change
-            iconEye.classList.add('ri-eye-line')
-            iconEye.classList.remove('ri-eye-off-line')
-        } else{
-            // Change to password
-            input.type = 'password'
+const app = initializeApp(appSetting)
+const database = getDatabase(app)
+const users_in_db = ref(database, "users")
 
-            // Icon change
-            iconEye.classList.remove('ri-eye-line')
-            iconEye.classList.add('ri-eye-off-line')
+onValue(users_in_db, function(snapshort) {
+    let users_array = Object.values(snapshort.val())
+    console.log(users_array)
+})
+
+let success = "no";
+
+let form = document.querySelector("#container")
+let user_name = document.querySelector("#Username")
+let password = document.querySelector("#password")
+
+form.addEventListener("submit", (event) => {
+    formValidation();
+    if (isFormValid()) {
+        form.submit()
+    }
+    else{
+        event.preventDefault();
+    }
+})
+
+user_name.addEventListener("change", (event) => {
+    username_validation()
+    
+})
+
+password.addEventListener("change", (event) => {
+    password_validation()
+    
+})
+
+function isFormValid(){
+    let input_box = document.querySelectorAll(".input_box")
+    let result = true
+    input_box.forEach((box) => {
+        if (box.classList.contains("error")) {
+            result = false
+        }
+    });
+    return result
+}
+
+function formValidation(){
+    get_data_form_db()
+}
+
+function setError(element, message){
+    const parent = element.parentElement;
+    if (parent.classList.contains("success")) {
+        parent.classList.remove("success")
+    }
+    parent.classList.add("error")
+    const para = parent.querySelector("p")
+    para.textContent = message
+}
+
+function setSuccess(element, message) {
+    const parent = element.parentElement;
+    if (parent.classList.contains("error")) {
+        parent.classList.remove("error")
+    }
+    parent.classList.add("success")
+    const para = parent.querySelector("p")
+    para.textContent = message
+}
+
+
+function get_data_form_db(){
+    onValue(users_in_db, function(snapshort) {
+        let users_array = Object.values(snapshort.val())
+        console.log(users_array)
+        let user_name_value = user_name.value
+        let password_value = password.value
+
+        for (let i = 0; i < users_array.length; i++) {
+            let current_user = users_array[i]
+            console.log(current_user)
+            console.log(user_name_value, current_user.userName)
+            console.log(password_value, current_user.password)
+            if(success == "no"){
+                if(user_name_value == current_user.userName && password_value == current_user.password){
+                    setSuccess(user_name, "success")
+                    setSuccess(password, "success")
+                    // window.location.href = "http://127.0.0.1:5500/html/index.html";
+                    success = "yes"
+                }
+                else{
+                    setError(user_name, "invalid username or")
+                    setError(password, "invalid password")
+                }
+            }
         }
     })
 }
-
-showHiddenPass('login-pass','login-eye')
